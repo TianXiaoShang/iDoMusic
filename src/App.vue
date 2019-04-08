6abb7f7502e041d4ed212e56252fa93361ef376c
TianXiaoShang <1040418011@qq.com>
2019-04-09 02:17:44 +0800

    2

--- src/App.vue
+++ src/App.vue
@@ -52,38 +52,39 @@ export default {
     margin 0
     padding 0
     
-  body
-      position relative
-      overflow-y hidden
-
-    #app
-      position fixed
-      top 0
-      bottom 0
-      width 100%
+    body
+      height 100%
       overflow hidden
-      .slide-right-enter-active,
-      .slide-right-leave-active,
-      .slide-left-enter-active,
-      .slide-left-leave-active {
-        will-change: transform;
-        transition: all 500ms;
-        position: absolute;
-      }
-      .slide-right-enter {
-        opacity: 0;
-        transform: translate3d(-100%,-0, 0);
-      }
-      .slide-right-leave-active {
-        opacity: 0;
-        transform: translate3d(100%, 0, 0);
-      }
-      .slide-left-enter {
-        opacity: 0;
-        transform: translate3d(100%, 0, 0);
-      }
-      .slide-left-leave-active {
-        opacity: 0;
-        transform: translate3d(-100%,-0, 0);
-      }
+      width 100%
+      position fixed
+      #app
+        position fixed
+        top 0
+        bottom 0
+        width 100%
+        overflow hidden
+        .slide-right-enter-active,
+        .slide-right-leave-active,
+        .slide-left-enter-active,
+        .slide-left-leave-active {
+          will-change: transform;
+          transition: all 500ms;
+          position: absolute;
+        }
+        .slide-right-enter {
+          opacity: 0;
+          transform: translate3d(-100%,-0, 0);
+        }
+        .slide-right-leave-active {
+          opacity: 0;
+          transform: translate3d(100%, 0, 0);
+        }
+        .slide-left-enter {
+          opacity: 0;
+          transform: translate3d(100%, 0, 0);
+        }
+        .slide-left-leave-active {
+          opacity: 0;
+          transform: translate3d(-100%,-0, 0);
+        }
 </style>
