58b7d5f06ad4204af71074d924aa2cdb7dd3b104
TianXiaoShang <1040418011@qq.com>
2019-04-09 10:45:23 +0800

    bing

--- src/base/songList.vue
+++ src/base/songList.vue
@@ -86,7 +86,7 @@ export default {
         .listItem
             display inline-block
             box-sizing border-box
-            margin 2px 1% 6px 1%
+            margin 50px 1% 6px 1%
             vertical-align top
             // overflow hidden
             .coverWrap
